a653e1335a72423d219e2518b4e0336554e74ef6
Daniel Sepp <seppdaniel@gmail.com>
2021-02-05 01:03:39 +0200

    Fire Model events before and after locking and unlocking

--- src/Lockable.php
+++ src/Lockable.php
@@ -7,14 +7,23 @@ use Envant\EloquentLockable\Exceptions\UpdatingLockedException;
 
 trait Lockable
 {
+
     /**
      * @return mixed
      */
     public function lockUpdating()
     {
+        if ($this->fireModelEvent('lockingUpdates') === false) {
+            return false;
+        }
+
         $this[config('lockable.locked_updating_column')] = true;
 
-        return $this->save();
+        $result = $this->save();
+
+        $this->fireModelEvent('lockedUpdates', false);
+
+        return $result;
     }
 
     /**
@@ -22,9 +31,17 @@ trait Lockable
      */
     public function unlockUpdating()
     {
+        if ($this->fireModelEvent('unlockingUpdates') === false) {
+            return false;
+        }
+
         $this[config('lockable.locked_updating_column')] = false;
 
-        return $this->save();
+        $result = $this->save();
+
+        $this->fireModelEvent('unlockedUpdates', false);
+
+        return $result;
     }
 
     /**
@@ -32,9 +49,17 @@ trait Lockable
      */
     public function lockDeleting()
     {
+        if ($this->fireModelEvent('lockingDeletes') === false) {
+            return false;
+        }
+
         $this[config('lockable.locked_deletion_column')] = true;
 
-        return $this->save();
+        $result = $this->save();
+
+        $this->fireModelEvent('lockedDeletes', false);
+
+        return $result;
     }
 
     /**
@@ -42,21 +67,69 @@ trait Lockable
      */
     public function unlockDeleting()
     {
+        if ($this->fireModelEvent('unlockingDeletes') === false) {
+            return false;
+        }
+
         $this[config('lockable.locked_deletion_column')] = false;
 
-        return $this->save();
+        $result = $this->save();
+
+        $this->fireModelEvent('lockedDeletes', false);
+
+        return $result;
+    }
+
+    public static function lockingUpdates($callback)
+    {
+        static::registerModelEvent('lockingUpdates', $callback);
+    }
+
+    public static function lockedUpdates($callback)
+    {
+        static::registerModelEvent('lockedUpdates', $callback);
+    }
+
+    public static function unlockingUpdates($callback)
+    {
+        static::registerModelEvent('unlockingUpdates', $callback);
+    }
+
+    public static function unlockedUpdates($callback)
+    {
+        static::registerModelEvent('unlockedUpdates', $callback);
+    }
+
+    public static function lockingDeletes($callback)
+    {
+        static::registerModelEvent('lockingDeletes', $callback);
+    }
+
+    public static function lockedDeletes($callback)
+    {
+        static::registerModelEvent('lockedDeletes', $callback);
+    }
+
+    public static function unlockingDeletes($callback)
+    {
+        static::registerModelEvent('unlockingDeletes', $callback);
+    }
+
+    public static function unlockedDeletes($callback)
+    {
+        static::registerModelEvent('unlockedDeletes', $callback);
     }
 
     public static function bootLockable()
     {
         static::updating(function ($model) {
-            if ((bool) $model[config('lockable.locked_updating_column')]) {
+            if ((bool)$model[config('lockable.locked_updating_column')]) {
                 throw new UpdatingLockedException();
             }
         });
 
         static::deleting(function ($model) {
-            if ((bool) $model[config('lockable.locked_deletion_column')]) {
+            if ((bool)$model[config('lockable.locked_deletion_column')]) {
                 throw new DeletingLockedException();
             }
         });
